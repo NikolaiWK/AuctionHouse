26ce112ed338bc373871aef23ebf0f583385c27f
NikolaiWK <nikolaiwolhardt@gmail.com>
2024-12-18 15:21:43 +0100

    Tilføje mock auction time job

---
+++ src/AuctionTimerJob/AuctionHouse.AuctionTimerJob.Job/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+/*Step by step*/
+//Receive rabbit with endtime
+
+//Create database entity
+
+//Do cron scheduled job every 1 minute (or faster)...
+
+//Check database for endtimes to exceed current time and then make rabbitmq event back to action management service to tell auction has ended.
+
+//Delete the entity afterwards
